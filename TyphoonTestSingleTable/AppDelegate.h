d38223bd3de5bcf83e14bb2ebfbd308d0dfca0bb
Reutskiy <reutskiy@MacBook-Pro-Reutskiy.local>
2017-06-07 16:48:41 +0300

    Initial Commit

---
+++ TyphoonTestSingleTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TyphoonTestSingleTable
+//
+//  Created by Reutskiy on 07.06.17.
+//  Copyright © 2017 Reutskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
